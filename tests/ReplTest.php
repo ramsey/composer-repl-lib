1a7078e7db0b30c9301364fe3b87337ce91a519f
Ben Ramsey <ben@benramsey.com>
2022-04-16 19:39:44 -0500

    feat: add getConfig() and getScopeVariables() to Repl (#3)

--- tests/ReplTest.php
+++ tests/ReplTest.php
@@ -6,12 +6,15 @@ namespace Ramsey\Test\Dev\Repl;
 
 use Composer\Factory;
 use Composer\IO\ConsoleIO;
+use PHPUnit\Framework\TestCase as PhpUnitTestCase;
+use Psy\Configuration;
 use Psy\Shell;
 use Ramsey\Dev\Repl\Process\ProcessFactory;
 use Ramsey\Dev\Repl\Repl;
 use Symfony\Component\Console\Helper\HelperSet;
 use Symfony\Component\Console\Input\StringInput;
 use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\Console\Output\NullOutput;
 use Symfony\Component\Process\Exception\ProcessTimedOutException;
 
 use function dirname;
@@ -26,6 +29,24 @@ use const PHP_OS_FAMILY;
 
 class ReplTest extends TestCase
 {
+    private Repl $repl;
+
+    protected function setUp(): void
+    {
+        $input = new StringInput('');
+        $output = Factory::createOutput();
+        $helperSet = new HelperSet();
+        $io = new ConsoleIO($input, $output, $helperSet);
+
+        $composerFile = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'composer.json';
+        $composer = Factory::create($io, $composerFile);
+
+        $repositoryRoot = (string) realpath(dirname($composerFile));
+        $processFactory = new ProcessFactory();
+
+        $this->repl = new Repl($repositoryRoot, $processFactory, $composer, false);
+    }
+
     public function testReplCommand(): void
     {
         if (PHP_OS_FAMILY === 'Windows') {
@@ -90,20 +111,56 @@ class ReplTest extends TestCase
         $expected = implode("\n", $lines);
 
         $input = new StringInput('');
-        $output = Factory::createOutput();
-        $helperSet = new HelperSet();
-        $io = new ConsoleIO($input, $output, $helperSet);
-
-        $composerFile = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'composer.json';
-        $composer = Factory::create($io, $composerFile);
-
-        $repositoryRoot = (string) realpath(dirname($composerFile));
-        $processFactory = new ProcessFactory();
         $bufferedOutput = new BufferedOutput();
 
-        $repl = new Repl($repositoryRoot, $processFactory, $composer, false);
-        $repl->run($input, $bufferedOutput);
+        $this->repl->run($input, $bufferedOutput);
 
         $this->assertSame($expected, $bufferedOutput->fetch());
+    }
+
+    /**
+     * This test is primarily for coverage purposes when running on
+     * GitHub Actions. Since {@see testReplRun()} will not pass when
+     * running on GitHub Actions, we use this test to execute the same
+     * code using {@see NullOutput} so that the coverage reports see
+     * {@see Repl} as fully covered.
+     */
+    public function testRunExecutesWithoutErrors(): void
+    {
+        $input = new StringInput('');
+        $nullOutput = new NullOutput();
+
+        $this->repl->run($input, $nullOutput);
+
+        $this->assertInstanceOf(Configuration::class, $this->repl->getConfig());
+    }
+
+    public function testStartUpMessageIsSetOnTheConfig(): void
+    {
+        $expected = <<<'EOD'
+            ------------------------------------------------------------------------
+            <info>Welcome to the development console (REPL) for ramsey/composer-repl-lib.</info>
+            <fg=cyan>To learn more about what you can do in PsySH, type `help`.</>
+            ------------------------------------------------------------------------
+            EOD;
+
+        $this->assertSame($expected, $this->repl->getConfig()->getStartupMessage());
+    }
+
+    public function testDefaultIncludesPropertyIsSetOnTheConfig(): void
+    {
+        $expected = ['repl.php'];
+
+        $this->assertSame($expected, $this->repl->getConfig()->getDefaultIncludes());
+    }
+
+    public function testGetScopeVariables(): void
+    {
+        $scopeVariables = $this->repl->getScopeVariables();
+
+        $this->assertArrayHasKey('env', $scopeVariables);
+        $this->assertIsArray($scopeVariables['env']);
+        $this->assertArrayHasKey('phpunit', $scopeVariables);
+        $this->assertInstanceOf(PhpUnitTestCase::class, $scopeVariables['phpunit']);
     }
 }
